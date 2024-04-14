e8f578e05b84297d039fe2f6ccbdeed944ab74be
Vaibhav Chauhan <137649284+vaibhavchauhantech@users.noreply.github.com>
2024-04-15 00:12:21 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+// frontend/src/App.js
+
+import React from 'react';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
